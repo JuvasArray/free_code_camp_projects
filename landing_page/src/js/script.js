c5e7ad1dbfe35a40f3d413d93312a0aa58f3ee39
achouraithamiche <achouraithamiche40@gmail.com>
2020-03-31 14:36:38 +0100

    Push

--- landing_page/src/js/script.js
+++ landing_page/src/js/script.js
@@ -1,4 +1,2 @@
-// coded by @atjonathan
 const projectName = 'product-landing-page';
 localStorage.setItem('example_project', 'Product Landing Page');
-// REPLACE CDN WITH GITCDN!!!
--- portfolio/src/js/script.js
+++ portfolio/src/js/script.js
@@ -1,2 +1,11 @@
+// Free Code Camp Testing Script
 const projectName = 'portfolio';
-localStorage.setItem('example_project', 'Tribute Page');
+localStorage.setItem('example_project', 'Tribute Page')
+
+
+// Navigation
+const navToggle = document.querySelector('.nav-toggle');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
